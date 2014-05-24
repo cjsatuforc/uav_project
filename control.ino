966567e81f6f0f3850df33626149693787314bf9
Stuart <sagriffiths87@gmail.com>
2014-05-24 20:17:16 +0100

    Control.ino created. Framework set out. Waiting for work to begin. Need to select sensor types and detail functionality.

---
+++ control.ino
@@ -0,0 +1,41 @@
+/*############################################################################
+#                                                                            #
+#                          Arduino Control Program                           #
+#                                                                            #
+#Description: Controls motors for UAV. Should respond to various sensor      #
+#             inputs like a gyroscope as well as commands from an external   #
+#             device.                                                        #
+#Inputs: TBC                                                                 #
+#Outputs: TBC                                                                #
+#Written by: Stuart Griffiths (sagriffiths87@gmail.com)                      #
+#Project details: https://github.com/sagriff/uav_project/                    #
+#License: Apache Version 2.0 http://www.apache.org/licenses/                 #
+############################################################################*/
+
+
+//If you use any of this code, then please let me know. It would be great for
+//my work to help others.
+
+
+//VARIABLES  
+//#########
+
+
+
+//Setup Function
+//##############
+
+void setup() {
+
+
+
+}
+
+//Loop Function
+//#############
+
+void loop() {
+
+
+
+}
